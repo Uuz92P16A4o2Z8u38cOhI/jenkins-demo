66c481b184955c3f331abcfa040831b14a538dd0
hyl <2546615650@qq.com>
2020-06-23 09:53:39 +0800

    "test"

---
+++ demo/src/main/java/hyl/jenkins/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package hyl.jenkins.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping("say")
+    public String say() {
+        return "hello!!";
+    }
+}
